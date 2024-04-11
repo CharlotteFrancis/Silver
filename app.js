1a11fbcae91cf60dee2b2a404e20d34ef43cbb0f
CharlotteFrancis <ChiharuTaru@gmail.com>
2024-04-11 13:34:19 -0700

    Testing functions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Card Class
 class Card {
-  constructor(name, set, rarity, grace, valor, charm, mystique, ferocity) {
+  constructor(name, set, rarity, grace, valor, charm, mystique, ferocity, overall) {
       this.name = name
       this.set = set
       this.rarity = rarity
@@ -9,16 +9,87 @@ class Card {
       this.charm = charm
       this.mystique = mystique
       this.ferocity = ferocity
+      this.overall = overall
     }
 }
 
 //List of Cards
 const cardList = []
 
+function test() {
+
+  sortCharm()
+
+  for(let i = 0; i < 50; i++) {
+    cardList.shift()
+  }
+  sortValor()
+  output()
+}
+
+function output() {
+  const dataContainer = document.getElementById('data')
+  const table = document.createElement('table')
+
+  if (cardList.length >= 50) {
+      for(let i = 0; i < 50; i++) {
+        console.log(cardList[i])
+
+        const tr = document.createElement('tr')
+
+        const td = document.createElement('td')
+        td.textContent = " "
+        const tdName = document.createElement('td')
+        const tdSet = document.createElement('td')
+        const tdRarity = document.createElement('td')
+
+        tdName.textContent = cardList[i].name
+        tr.appendChild(tdName)
+        tr.appendChild(td)
+        tdSet.textContent = cardList[i].set
+        tr.appendChild(tdSet)
+        tr.appendChild(td)
+        tdRarity.textContent = cardList[i].rarity
+        tr.appendChild(tdRarity)
+        tr.appendChild(td)
+
+        table.appendChild(tr)
+    
+        dataContainer.appendChild(table);
+      }
+  } else {
+    dataContainer.textContent = 'Not enough data found.'
+  }
+}
+
+function sortOverall() {
+  cardList.sort((a, b) => b.overall - a.overall)
+}
+
+function sortGrace() {
+  cardList.sort((a, b) => b.grace - a.grace)
+}
+
+function sortValor() {
+  cardList.sort((a, b) => b.valor - a.valor)
+}
+
+function sortCharm() {
+  cardList.sort((a, b) => b.charm - a.charm)
+}
+
+function sortMystique() {
+  cardList.sort((a, b) => b.mystique - a.mystique)
+}
+
+function sortFerocity() {
+  cardList.sort((a, b) => b.ferocity - a.ferocity)
+}
+
 //Code to get data from sheet
 function handleGoogleSheetData(data) {
   const values = data.values;
-  const dataContainer = document.getElementById('data');
+  // const dataContainer = document.getElementById('data');
 
   console.log(values)
 
@@ -35,35 +106,38 @@ function handleGoogleSheetData(data) {
           // });
           // table.appendChild(tr);
 
-          if (row[4] != "None") {
-            cardList.push({
-              name: row[0],
-              set: row[1],
-              rarity: row[3],
-              grace: row[4],
-              valor: row[5],
-              charm: row[6],
-              mystique: row[7],
-              ferocity: row[8],
-            })
+          if (row[4] != "None" && row[0] != "Name") {
+
+            for (let i = 0; i < parseInt(row[9]); i++) {
+              cardList.push({
+                name: row[0],
+                set: row[1],
+                rarity: row[3],
+                grace: parseInt(row[4]),
+                valor: parseInt(row[5]),
+                charm: parseInt(row[6]),
+                mystique: parseInt(row[7]),
+                ferocity: parseInt(row[8]),
+                overall: parseInt(row[4]) + parseInt(row[5]) + parseInt(row[6]) + parseInt(row[7]) + parseInt(row[8])
+              })
+            }
           }
       });
+
       // dataContainer.appendChild(table);
+
+      test()
   } else {
       // dataContainer.textContent = 'No data found.';
       Console.log('No data found.')
   }
-
-  //test
-  cardList.sort((a, b) => b.charm - a.charm)
-  console.log(cardList[1])
 }
 
 function fetchGoogleSheetData() {
   const apiKey = API_KEY // API key
   const spreadsheetId = '1il95C-Ic0tvQA-x98Kj6MiqyyVCLOyarppX6t7FFCWM' // spreadsheet ID
   const sheetName = 'data_file' // sheet name
-  const range = 'A1:I1457' // Replace with the range you want to read
+  const range = 'A1:J1457' // Replace with the range you want to read
 
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}!${range}?key=${apiKey}&callback=handleGoogleSheetData`
 
@@ -76,4 +150,4 @@ function fetchGoogleSheetData() {
 }
 
 // Fetch Google Sheets data when the page loads
-window.onload = fetchGoogleSheetData;
+window.onload = fetchGoogleSheetData
